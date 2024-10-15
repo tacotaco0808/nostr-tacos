3e9fc434d10470bfb344e05c833379987cfe7bd0
TakutoMatsui <takuto1488@gmail.com>
2024-10-15 19:05:24 +0900

    #1 ユーザーデータの送信とリレーインスタンスの初期化

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,25 +1,60 @@
-import { generateSecretKey } from "nostr-tools";
-import { useRef, useState } from "react";
+import { finalizeEvent, generateSecretKey, Relay } from "nostr-tools";
+import { useEffect, useRef, useState } from "react";
 const RegisterForm = () => {
   const inputUserName = useRef<HTMLInputElement | null>(null);
-  const [userName, setUserName] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [sK, setSK] = useState<Uint8Array | null>(null);
+  const relayRef = useRef<Relay | null>(null);
   const generateNewSK = () => {
     const newSK = generateSecretKey();
     setSK(newSK);
     return newSK;
   };
-
-  const createUserHandle = () => {
+  const initializeRelay = async () => {
+    try {
+      const relay = await Relay.connect("wss://relay-jp.nostr.wirednet.jp");
+      relayRef.current = relay;
+      console.log(`connected to ${relay.url}`);
+    } catch (error) {
+      console.error("Error connecting to relay:", error);
+    }
+  };
+  const sendUserDataToRelay = async (secreteKey: Uint8Array | null) => {
+    if (secreteKey) {
+      // ユーザーデータ
+      const event = finalizeEvent(
+        {
+          kind: 0,
+          created_at: Math.floor(Date.now() / 1000),
+          tags: [],
+          content: `{"name":"${inputUserName.current?.value}","display_name":"${inputUserName.current?.value}","picture":"https://bananas"}`,
+        },
+        secreteKey
+      );
+      await relayRef.current?.publish(event);
+    } else {
+      console.error("Error sending data to relay:");
+      return;
+    }
+  };
+  const createUserHandle = async () => {
     if (inputUserName.current && inputUserName.current.value) {
-      setUserName(inputUserName.current.value);
       setError(null);
       const secretKey = generateNewSK();
+      await sendUserDataToRelay(secretKey);
     } else {
       setError("名前を入力してください");
     }
   };
+  useEffect(() => {
+    initializeRelay();
+    return () => {
+      //コンポーネントアンマウント時接続を切る
+      if (relayRef.current) {
+        relayRef.current.close();
+      }
+    };
+  }, []);
   return (
     <>
       <h3>新しくユーザーを作成</h3>
