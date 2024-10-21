48dba170c61d378d40a8c18ed595b2290a2e03e6
tacotaco0808 <takuto1488@gmail.com>
2024-10-21 21:25:53 +0900

    #5 postをリレーから取得し、最新20件をリスト表示

--- src/components/ShowPosts.tsx
+++ src/components/ShowPosts.tsx
@@ -1,17 +1,19 @@
 import { Event, Filter, Relay } from "nostr-tools";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ShowPosts = () => {
   const relayRef = useRef<Relay | null>(null);
+  const [posts, setPosts] = useState<Event[]>([]);
   const getRelayPosts = async () => {
     try {
       const relay = await Relay.connect("wss://relay-jp.nostr.wirednet.jp");
       relayRef.current = relay;
       console.log(`connected to ${relay.url}`);
-      const filter: Filter = { kinds: [1] };
+      const filter: Filter = { kinds: [1], limit: 20 };
       const subscription = relay.subscribe([filter], {
         onevent(event: Event) {
           console.log("event:", event);
+          setPosts((prevPosts) => [event, ...prevPosts]);
         },
         oneose() {
           //eoseは保存しているイベントをすべて吐き出した後出されるイベント
@@ -31,6 +33,19 @@ const ShowPosts = () => {
       }
     };
   }, []);
-  return <>aiuepo</>;
+  return (
+    <>
+      aiuepo
+      <ul>
+        {posts.map((post, index) => {
+          return (
+            <li key={post.id}>
+              {index + 1}:{post.content}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 export default ShowPosts;
