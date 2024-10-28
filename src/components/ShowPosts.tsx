7a5d99c7b592671d3d99caf015cb4af0c917491b
tacotaco0808 <takuto1488@gmail.com>
2024-10-28 22:02:49 +0900

    #5 poolを使った投稿の取得

--- src/components/ShowPosts.tsx
+++ src/components/ShowPosts.tsx
@@ -1,8 +1,7 @@
-import { Event, Filter, Relay, SimplePool } from "nostr-tools";
-import { useEffect, useRef, useState } from "react";
+import { Event, Filter, SimplePool } from "nostr-tools";
+import { useEffect, useState } from "react";
 
 const ShowPosts = () => {
-  const relayRef = useRef<Relay[] | null>([]);
   const [posts, setPosts] = useState<Event[]>([]);
   const [isLoadingPosts, setIsLoadingPosts] = useState<boolean>(true);
   const [replayPosts, setReplayPosts] = useState<Event[]>([]);
@@ -24,7 +23,8 @@ const ShowPosts = () => {
         setPosts((prevPosts) => [...prevPosts, event]);
       },
       oneose() {
-        subscription.close;
+        subscription.close();
+        setIsLoadingPosts(false);
       },
     });
     return () => {
@@ -32,79 +32,30 @@ const ShowPosts = () => {
       subscription.close();
     };
   }, []);
-  // /*リレーへ接続 */
-  // const connectRelays = async (urlRelays: string[]): Promise<Relay[]> => {
-  //   const relays: Relay[] = [];
-  //   for (const url of urlRelays) {
-  //     try {
-  //       const relay = await Relay.connect(url);
-  //       relays.push(relay);
-  //       console.log(`Connected to relay ${url}`);
-  //     } catch (error) {
-  //       console.log(`Failed to connect to relay ${url}`);
-  //     }
-  //   }
-  //   return relays;
-  // };
-  // /*すべての投稿を読み込む */
-  // const getRelaysPosts = async (relays: Relay[]) => {
-  //   try {
-  //     for (const relay of relays) {
-  //       const filter: Filter = { kinds: [1], limit: 100 };
-  //       const subscription = relay.subscribe([filter], {
-  //         onevent(event: Event) {
-  //           console.log("event:", event);
-  //           setPosts((prevPosts) => [event, ...prevPosts]);
-  //         },
-  //         oneose() {
-  //           //eoseは保存しているイベントをすべて吐き出した後出されるイベント
-  //           subscription.close();
-  //           setIsLoadingPosts(false);
-  //         },
-  //       });
-  //     }
-  //   } catch (error) {
-  //     console.error("Error connecting to relay:", error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   const relayURLs = [
-  //     "wss://relay-jp.nostr.wirednet.jp",
-  //     "punya",
-  //     "wss://relay-jp.nostr.wirednet.jp",
-  //   ];
-  //   const setupRelays = async () => {
-  //     const connectedRelays = await connectRelays(relayURLs);
-  //     relayRef.current = connectedRelays;
-  //   };
-  //   setupRelays();
-  //   return () => {
-  //     //コンポーネントアンマウント時接続を切る
-  //     relayRef.current?.forEach((relay) => relay.close());
-  //   };
-  // }, []);
-  // /*すべての投稿を読み込んだ後に実行 */
-  // useEffect(() => {
-  //   if (!isLoadingPosts) {
-  //     const tmpNormalPosts = posts.filter(
-  //       //通常の投稿が入っている
-  //       (post: Event) => !post.tags.some((tag) => tag[0] === "e")
-  //     );
-  //     const tmpReplaylPosts = posts.filter(
-  //       //返信の投稿が入っている
-  //       (post: Event) => post.tags.some((tag) => tag[0] === "e")
-  //     );
-  //     setNormalPosts((prevPosts: Event[]) => [...tmpNormalPosts, ...prevPosts]);
-  //     setReplayPosts((prevPosts: Event[]) => [
-  //       ...tmpReplaylPosts,
-  //       ...prevPosts,
-  //     ]);
-  //   }
-  // }, [isLoadingPosts]);
+
+  /*すべての投稿を読み込んだ後に実行 */
+  /*投稿が返信か、通常か仕分け */
+  useEffect(() => {
+    if (!isLoadingPosts) {
+      const tmpNormalPosts = posts.filter(
+        //通常の投稿が入っている
+        (post: Event) => !post.tags.some((tag) => tag[0] === "e")
+      );
+      const tmpReplaylPosts = posts.filter(
+        //返信の投稿が入っている
+        (post: Event) => post.tags.some((tag) => tag[0] === "e")
+      );
+      setNormalPosts((prevPosts: Event[]) => [...tmpNormalPosts, ...prevPosts]);
+      setReplayPosts((prevPosts: Event[]) => [
+        ...tmpReplaylPosts,
+        ...prevPosts,
+      ]);
+    }
+  }, [isLoadingPosts]);
   return (
     <>
       aiuepo
-      {/* <ul>
+      <ul>
         <h3>通常の投稿</h3>
         {normalPosts.map((post, index) => {
           return (
@@ -123,8 +74,9 @@ const ShowPosts = () => {
             </li>
           );
         })}
-      </ul> */}
-      <div>
+      </ul>
+      {/* debug */}
+      {/* <div>
         <h1>Nostr Events</h1>
         <ul>
           {posts.map((event) => (
@@ -135,7 +87,7 @@ const ShowPosts = () => {
             </li>
           ))}
         </ul>
-      </div>
+      </div> */}
     </>
   );
 };
