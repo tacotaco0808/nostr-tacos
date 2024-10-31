530bd8b894068ce36cd2256f38c3b34b4a67571d
tacotaco0808 <takuto1488@gmail.com>
2024-10-31 11:25:13 +0900

    #8 MUIのCssBaselineでリセットcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    {/*リセットcss */}
+    <CssBaseline />
+    <App />
+  </>
+);
