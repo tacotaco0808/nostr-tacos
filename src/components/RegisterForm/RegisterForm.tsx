fce614a98c61da7abe54f6fd50386a80096f627d
TakutoMatsui <takuto1488@gmail.com>
2024-11-29 17:48:18 +0900

    #18 ユーザー情報をlocalstorageに保存(仮)

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,6 +1,11 @@
 import { finalizeEvent, generateSecretKey, SimplePool } from "nostr-tools";
 import { useRef, useState } from "react";
 import styles from "./RegisterForm.module.scss";
+type UserData = {
+  name: string;
+  connectingRelays: string[];
+  secKey: Uint8Array;
+};
 const RegisterForm = () => {
   const inputUserName = useRef<HTMLInputElement | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -12,7 +17,16 @@ const RegisterForm = () => {
     return newSK;
   };
   const pool = new SimplePool();
-
+  const saveToLocalStorage = (key: string, data: any) => {
+    /*ローカルストレージへデータを保存 */
+    if (data.secKey instanceof Uint8Array) {
+      data.secKey = Array.from(data.secKey); // Uint8Arrayを配列化
+    }
+    localStorage.setItem(key, JSON.stringify(data));
+  };
+  const uint8ArrayToBase64 = (array: Uint8Array): string => {
+    return btoa(String.fromCharCode(...array));
+  };
   const sendUserDataToRelay = async (
     secreteKey: Uint8Array | null,
     userName: string,
@@ -38,9 +52,17 @@ const RegisterForm = () => {
   };
   const createUserHandle = async () => {
     if (inputUserName.current && inputUserName.current.value) {
-      setError(null);
       const currentUserName = inputUserName.current.value;
       const secretKey = generateNewSK();
+      const newUserData: UserData = {
+        name: currentUserName,
+        connectingRelays: [],
+        secKey: secretKey,
+      };
+      setError(null);
+      const encoded = uint8ArrayToBase64(secretKey);
+      saveToLocalStorage(encoded, newUserData);
+
       await sendUserDataToRelay(secretKey, currentUserName, relayURLs);
       inputUserName.current.value = "";
     } else {
